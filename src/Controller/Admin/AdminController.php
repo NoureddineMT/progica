d9e4d6d5b7fd3448eacf43a0af9bb82c010c0afb
Noureddine Mouatamid <95962747+NoureddineMT@users.noreply.github.com>
2022-06-12 11:55:44 +0200

    feat: CRUD Gite + fixtures

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -40,12 +40,54 @@ class AdminController extends AbstractController{
             $em = $doctrine->getManager();
             $em->persist($gite);
             $em->flush();
+            $this->addFlash('success', 'Votre gite a bien été créer');
             return $this->redirectToRoute("admin_index");
         }
 
         return $this->renderForm("admin/gite/create.html.twig", [
             "formGite" => $form
         ]);
+    }
+
+    /**
+     * @Route("admin/gite/edit/{id}", name="admin_gite_edit")
+     */
+
+    public function edit(Gite $gite, Request $request,ManagerRegistry $doctrine){
+
+        $form = $this->createForm(GiteType::class, $gite);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $em = $doctrine->getManager();
+            $em->flush();
+            $this->addFlash('success', 'Gite modifier avec succès');
+            return $this->redirectToRoute("admin_index");
+        }
+
+        return $this->renderForm("admin/gite/edit.html.twig", [
+            "formGite"=>$form
+        ]);
+    }
+
+    /**
+     * @Route("admin/gite/delet/{id}", name="admin_gite_delete")
+     */
+
+    public function delete(Gite $gite, ManagerRegistry $doctrine, Request $request){
+        
+        if($this->isCsrfTokenValid("gite_delete". $gite->getId(), $request->request->get('token'))){
+            $em = $doctrine->getManager();
+            $em->remove($gite);
+            $em->flush();
+            $this->addFlash('success', "Gite supprimé avec succès");
+        }else {
+            $this->addFlash('error', "Token non valide");
+        }
+        
+
+        return $this->redirectToRoute("admin_index");
     }
 }
 
